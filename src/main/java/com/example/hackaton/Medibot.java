97dda5339cdeeeca15407a4d60093ebc5f6c931c
Trysse <tim.cattrysse@gmail.com>
2024-12-12 21:25:21 +0100

    poes

--- src/main/java/com/example/hackaton/Medibot.java
+++ src/main/java/com/example/hackaton/Medibot.java
@@ -11,8 +11,7 @@ public interface Medibot {
              value = """
 You are a medical laboratory,
 your goal is to propose medical tests or analyses based on the symptoms given;
-Answer only with a list of possible clinical tests or analyses, nothing else
-With the following example: "Calcium","Ferritin","Kalium"
+Answer only with a list of possible clinical tests or analyses, ordered by relevance
 """
      )
      @UserMessage("Give me the clinical tests or analyses based on the following symptoms or medical conditions: {{message}}")
--- src/main/java/com/example/hackaton/ParsingConfiguration.java
+++ src/main/java/com/example/hackaton/ParsingConfiguration.java
@@ -3,6 +3,7 @@ package com.example.hackaton;
 import dev.langchain4j.data.document.Document;
 import dev.langchain4j.data.document.loader.FileSystemDocumentLoader;
 
+import dev.langchain4j.data.document.loader.UrlDocumentLoader;
 import dev.langchain4j.data.document.parser.apache.tika.ApacheTikaDocumentParser;
 import dev.langchain4j.data.segment.TextSegment;
 import dev.langchain4j.rag.content.retriever.ContentRetriever;
@@ -15,6 +16,7 @@ import org.springframework.context.annotation.Configuration;
 
 import java.io.File;
 import java.nio.file.Path;
+import java.util.List;
 
 @Slf4j
 @Configuration
@@ -27,7 +29,8 @@ public class ParsingConfiguration {
     @Bean
     public ContentRetriever contentRetriever(ApacheTikaDocumentParser apacheTikaDocumentParser) {
         // Here, we create an empty in-memory store for our documents and their embeddings.
-        InMemoryEmbeddingStore<TextSegment> embeddingStore = InMemoryEmbeddingStore.fromFile("vector-db.txt");
+//        InMemoryEmbeddingStore<TextSegment> embeddingStore = InMemoryEmbeddingStore.fromFile("vector-db.txt");
+//        InMemoryEmbeddingStore<TextSegment> embeddingStore = new InMemoryEmbeddingStore<>();
 
 //        log.info("Start ingesting the document");
 //        String path = ParsingConfiguration.class.getClassLoader().getResource("medical-tests-catalog.pdf").getPath();
@@ -37,6 +40,20 @@ public class ParsingConfiguration {
 //        EmbeddingStoreIngestor.ingest(document, embeddingStore);
 //        embeddingStore.serializeToFile("vector-db.txt");
 //        log.info("Done ingesting the document");
+
+        InMemoryEmbeddingStore<TextSegment> embeddingStore = new InMemoryEmbeddingStore<>();
+
+        List<String> analyses = List.of("creatine-kinase", "ferritin-blood-test", "glucose-in-urine-test");
+//        List<String> analyses = List.of("ferritin-blood-test");
+        for (String analyse : analyses) {
+            Document document = UrlDocumentLoader.load("https://medlineplus.gov/lab-tests/" + analyse + "/", apacheTikaDocumentParser);
+            // Here, we are ingesting our documents into the store.
+            // Under the hood, a lot of "magic" is happening, but we can ignore it for now.
+            EmbeddingStoreIngestor.ingest(document, embeddingStore);
+
+        }
+
+
 
 // Lastly, let's create a content retriever from an embedding store.
         return EmbeddingStoreContentRetriever.from(embeddingStore);
