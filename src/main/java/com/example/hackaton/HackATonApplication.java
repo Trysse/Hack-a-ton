bdc5d19496b55718e9d977bf4a11807a85c9ddcc
Trysse <tim.cattrysse@gmail.com>
2024-12-12 17:59:34 +0100

    Initial commit

---
+++ src/main/java/com/example/hackaton/HackATonApplication.java
@@ -0,0 +1,13 @@
+package com.example.hackaton;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HackATonApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HackATonApplication.class, args);
+    }
+
+}
